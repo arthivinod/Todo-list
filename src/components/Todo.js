d2106b660efa0ac5b5d68ae9650cb6d0e140cd52
arthivinod <arthivinod28@gmail.com>
2021-09-21 13:09:32 +0530

    First commit

---
+++ src/components/Todo.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import TodoForm from './TodoForm'
+import TodoList from './TodoList'
+
+function Todo() {
+
+    const[edit, setEdit]=useState({
+
+        id: null,
+        value:""
+    })
+
+
+    return todos.map((todo, index) => (
+        <div 
+            className={todo.isComplete ? 'todo-row complete' : 'todo-row' } 
+            key={index}
+        >
+
+        <div key={todo.id} onCLick={() => completeTodo(todo.id)} >
+            {todo.text}
+        </div>
+
+        <div className='icons'>
+        <RiCloseCircleLine
+          onClick={() => removeTodo(todo.id)}
+          className='delete-icon'
+        />
+
+        </div>
+
+        </div>
+    ));
+}
+
+export default Todo;
